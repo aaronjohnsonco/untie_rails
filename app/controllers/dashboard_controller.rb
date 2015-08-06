151c4b0c6a0a556b188f6ea9afc97b7459826307
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-08-05 22:21:34 -0500

    add auth

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action do
+    redirect_to root_path unless current_user && current_user.admin?
+  end
+
   def index
   	@posts = Post.order('pub_date desc').limit(4)
     @questions = Question.order('created_at desc').limit(4)
@@ -31,5 +37,6 @@ class DashboardController < ApplicationController
   def prospects
     @prospects = Prospect.order('created_at desc')
   end
+
 
 end
