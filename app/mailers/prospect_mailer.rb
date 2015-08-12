17c953de1f945d72c0b7d734168bfdce729e42e5
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-08-11 23:04:41 -0500

    edit text

--- app/mailers/prospect_mailer.rb
+++ app/mailers/prospect_mailer.rb
@@ -2,17 +2,17 @@ class ProspectMailer < ActionMailer::Base
 
 	def basic_divorce_email(prospect)
 		@prospect = prospect
-		mail(to: "aaronjohnsonis@gmail.com", subject: "Basic Divorce Prospect", from: @prospect.email)
+		mail(to: "cchadwick@sbdlaw.net", subject: "Basic Divorce Prospect", from: @prospect.email)
 	end
 
 	def divorce_property_email(prospect)
 		@prospect = prospect
-		mail(to: "aaronjohnsonis@gmail.com", subject: "Divorce with Property Prospect", from: @prospect.email)
+		mail(to: "cchadwick@sbdlaw.net", subject: "Divorce with Property Prospect", from: @prospect.email)
 	end
 
 	def divorce_children_email(prospect)
 		@prospect = prospect
-		mail(to: "aaronjohnsonis@gmail.com", subject: "Divorce with Children Prospect", from: @prospect.email)
+		mail(to: "cchadwick@sbdlaw.net", subject: "Divorce with Children Prospect", from: @prospect.email)
 	end
 	
 end
