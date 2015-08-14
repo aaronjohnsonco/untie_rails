327966f2a3cf22bfdd69335dfcf62ba17005446d
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-08-14 11:45:16 -0500

    add email addresses

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,12 +2,14 @@ class ContactMailer < ActionMailer::Base
   
   def contact_email(contact)
   	@contact = contact
-  	mail(to: "aaronjohnsonis@gmail.com", subject: "New Contact", from: @contact.email)
+  	emails = ['aaronjohnsonis@gmail.com', 'cchadwick@sbdlaw.net']
+  	mail(to: emails, subject: "New Contact", from: @contact.email)
 	end
 
 	def question_email(contact)
 		@contact = contact
-		mail(to: "aaronjohnsonis@gmail.com", subject: "New Question Asked", from: @contact.email)
+		emails = ['aaronjohnsonis@gmail.com', 'cchadwick@sbdlaw.net']
+		mail(to: emails, subject: "New Question Asked", from: @contact.email)
 	end
 
 end
