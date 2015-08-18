27edc5af95cd4880ebba3c0904903e962a365d07
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-08-17 21:47:03 -0500

    post order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,33 +1,33 @@
 class PagesController < ApplicationController
 
   def home
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
   end
 
   def about
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
   end
 
   def faq
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
     @question_open = Question.limit(1).where('status = ?', 'published')
     @questions_closed = Question.offset(1).where('status = ?', 'published')
 	end
 
 	def bio
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
 	end
 
 	def basic
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
 	end
 
   def property
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
   end
 
   def children
-    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').limit(4)
+    @footer_posts = Post.where('pub_date <= ? AND status = ?', Date.today, 'published').order('pub_date desc').limit(4)
   end
 
   def download_pdf
