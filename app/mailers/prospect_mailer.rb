9b55a2a7856d04815f63e1c2e937d26a4e0023df
Aaron Johnson <aaronjohnsonis@gmail.com>
2015-08-27 07:54:26 -0500

    test modal

--- app/mailers/prospect_mailer.rb
+++ app/mailers/prospect_mailer.rb
@@ -15,7 +15,8 @@ class ProspectMailer < ActionMailer::Base
 	def divorce_children_email(prospect)
 		@prospect = prospect
 		emails = ['cchadwick@sbdlaw.net', 'Vcolvin@sbdlaw.net', 'aaron@bigmooncreative.com']
-		mail(to: emails, subject: "Divorce with Children Prospect", from: @prospect.email)
+		email = 'aaronjohnsonis@gmail.com'
+		mail(to: email, subject: "Divorce with Children Prospect", from: @prospect.email)
 	end
 
 	def contested_divorce_email(prospect)
