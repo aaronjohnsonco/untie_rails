d1738bc479a8067e459afa14d3cf3c72b999d65f
Aaron Johnson <aaronjohnsonis@gmail.com>
2016-12-27 16:27:55 -0600

    update contact info

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,13 +2,13 @@ class ContactMailer < ActionMailer::Base
 
   def contact_email(contact)
   	@contact = contact
-  	emails = ['aaronjohnsonis@gmail.com', 'admin@Chadwickandtignor.com']
+  	emails = ['connie@Chadwickandtignor.com']
   	mail(to: emails, subject: "New Contact", from: @contact.email)
 	end
 
 	def question_email(contact)
 		@contact = contact
-		emails = ['aaronjohnsonis@gmail.com', 'admin@Chadwickandtignor.com']
+		emails = ['connie@chadwickandtignor.com']
 		mail(to: emails, subject: "New Question Asked", from: @contact.email)
 	end
 
--- app/mailers/prospect_mailer.rb
+++ app/mailers/prospect_mailer.rb
@@ -2,41 +2,41 @@ class ProspectMailer < ActionMailer::Base
 
 	def basic_divorce_email(prospect)
 		@prospect = prospect
-		emails = ['admin@Chadwickandtignor.com', 'aaron@bigmooncreative.com']
+		emails = ['connie@chadwickandtignor.com']
 		mail(to: emails, subject: "Basic Divorce Prospect", from: @prospect.email)
 	end
 
 	def divorce_property_email(prospect)
 		@prospect = prospect
-		emails = ['admin@Chadwickandtignor.com', 'aaron@bigmooncreative.com']
+		emails = ['connie@chadwickandtignor.com']
 		mail(to: emails, subject: "Divorce with Property Prospect", from: @prospect.email)
 	end
 
 	def divorce_children_email(prospect)
 		@prospect = prospect
-		emails = ['admin@Chadwickandtignor.com', 'aaron@bigmooncreative.com']
+		emails = ['connie@chadwickandtignor.com']
 		mail(to: emails, subject: "Divorce with Children Prospect", from: @prospect.email)
 	end
 
 	def contested_divorce_email(prospect)
 		@prospect = prospect
-		emails = ['admin@Chadwickandtignor.com', 'aaron@bigmooncreative.com']
+		emails = ['connie@chadwickandtignor.com']
 		mail(to: emails, subject: "Divorce with Children Prospect", from: @prospect.email)
 	end
 
 	def basic_divorce_client(prospect)
 		@prospect = prospect
-		mail(to: @prospect.email, subject: "Basic Divorce", from: "admin@Chadwickandtignor.com")
+		mail(to: @prospect.email, subject: "Basic Divorce", from: "connie@chadwickandtignor.com")
 	end
 
 	def property_divorce_client(prospect)
 		@prospect = prospect
-		mail(to: @prospect.email, subject: "Divorce with Property", from: "admin@Chadwickandtignor.com")
+		mail(to: @prospect.email, subject: "Divorce with Property", from: "connie@chadwickandtignor.com")
 	end
 
 	def children_divorce_client(prospect)
 		@prospect = prospect
-		mail(to: @prospect.email, subject: "Divorce with Children", from: "admin@Chadwickandtignor.com")
+		mail(to: @prospect.email, subject: "Divorce with Children", from: "connie@chadwickandtignor.com")
 	end
 
 
